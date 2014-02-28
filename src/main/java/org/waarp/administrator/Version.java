a8f5c4cce37f98174f86c33bda9c6633de324ad2
Frederic Bregier <fredbregier@free.fr>
2014-02-28 14:44:03 +0100

    Fix dependencies and update version

--- src/main/java/org/waarp/administrator/Version.java
+++ src/main/java/org/waarp/administrator/Version.java
@@ -3,7 +3,7 @@ package org.waarp.administrator;
 /** Provides the version information of Waarp Password. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "1.0.7";
+ public static final String ID = "1.0.8";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
